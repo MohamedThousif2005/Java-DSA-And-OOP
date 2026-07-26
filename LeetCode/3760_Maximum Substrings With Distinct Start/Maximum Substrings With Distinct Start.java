7cda732780a56a98f9d77a2e24b814c706d8b69c
Mohamed Thousif <mohamedthousif2005@gmail.com>
2026-07-26 10:48:29 +0530

    String

---
+++ LeetCode/3760_Maximum Substrings With Distinct Start/Maximum Substrings With Distinct Start.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxDistinct(String s) {
+        int freq[] = new int[26];
+        int count = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            freq[s.charAt(i)-'a']++;
+            if(freq[s.charAt(i)-'a']==1) count++;
+        }
+        return count;
+    }
+}
