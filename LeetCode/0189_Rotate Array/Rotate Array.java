28616b8eb346caec392afb0666274fb16f7add3e
Mohamed Thousif <mohamedthousif2005@gmail.com>
2026-08-01 21:46:50 +0530

    Problem Description

---
+++ LeetCode/0189_Rotate Array/Rotate Array.java
@@ -0,0 +1,72 @@
+<div align="center">
+
+# 🔍 Rotate Array
+
+[![LeetCode](https://img.shields.io/badge/LeetCode-0189-orange?style=for-the-badge&logo=leetcode&logoColor=white)](https://leetcode.com/problems/rotate-array/)
+![Difficulty](https://img.shields.io/badge/Difficulty-Medium-orange?style=for-the-badge)
+![Topics](https://img.shields.io/badge/Topics-Array%20%7C%20Math%20%7C%20Two%20Pointers-blue?style=for-the-badge)
+![Acceptance](https://img.shields.io/badge/Acceptance-~43%25-yellow?style=for-the-badge)
+
+🔗 [View on LeetCode](https://leetcode.com/problems/rotate-array/)
+
+</div>
+
+---
+
+## 📋 Problem Statement
+
+Given an integer array `nums`, rotate the array to the **right** by `k` steps, where `k` is **non-negative**.
+
+---
+
+## 🧪 Examples
+
+### Example 1
+
+```text
+Input:  nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+
+Explanation:
+Rotate 1 step to the right: [7,1,2,3,4,5,6]
+Rotate 2 steps to the right: [6,7,1,2,3,4,5]
+Rotate 3 steps to the right: [5,6,7,1,2,3,4]
+```
+
+### Example 2
+
+```text
+Input:  nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+
+Explanation:
+Rotate 1 step to the right: [99,-1,-100,3]
+Rotate 2 steps to the right: [3,99,-1,-100]
+```
+
+---
+
+## ⚙️ Constraints
+
+| Constraint | Value |
+|---|---|
+| Array Length | `1 <= nums.length <= 10⁵` |
+| Element Range | `-2³¹ <= nums[i] <= 2³¹ - 1` |
+| Rotation Steps | `0 <= k <= 10⁵` |
+
+---
+
+## 🏷️ Tags
+
+`Array` &nbsp;&nbsp; `Math` &nbsp;&nbsp; `Two Pointers` &nbsp;&nbsp; `Medium`
+
+---
+
+<div align="center">
+
+**[⬅️ Back to Problems](../README.md)**
+<br/>
+
+*Happy Coding! 🚀*
+
+</div>
