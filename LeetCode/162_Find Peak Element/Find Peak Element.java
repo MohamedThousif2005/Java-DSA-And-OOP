397bb3671122ba4fc27d68ffa8e4d47531c70718
Mohamed Thousif <mohamedthousif2005@gmail.com>
2026-08-01 21:41:50 +0530

    Array

---
+++ LeetCode/162_Find Peak Element/Find Peak Element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        for(int i=nums.length-1;i>0;i--)
+        {
+            if(nums[i]>nums[i-1]) return i;
+        }
+        for(int i=1;i<nums.length-1;i++)
+        {
+            if(nums[i-1]<nums[i]&&nums[i+1]<nums[i]) return i;
+        }
+        return 0;
+    }
+}
